cbc0ddc638a6fef1477292acbf55f9e0630233d6
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-05 19:03:07 -0400

    me query to test authentication

--- users/schema.py
+++ users/schema.py
@@ -27,10 +27,18 @@ class CreateUser(graphene.Mutation):
 
 
 class Query(graphene.ObjectType):
+    me = graphene.Field(UserType)
     users = graphene.List(UserType)
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Not logged in!")
+
+        return user
 
 
 class Mutation(graphene.ObjectType):

62400983850f5c4ce8c9724ad48e4959f768e60b
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-05 10:23:18 -0400

    create links app and run migration

--- links/models.py
+++ links/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+    url = models.URLField()
+    # blank=True allows the field to be blank, default is set to False
+    description = models.TextField(blank=True)

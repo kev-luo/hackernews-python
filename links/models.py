c01178e43b2133e2c632f7a00ed8e1b84b8acd33
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-05 19:11:10 -0400

    relate link with user who created it

--- links/models.py
+++ links/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Link(models.Model):
     url = models.URLField()
     # blank=True allows the field to be blank, default is set to False
     description = models.TextField(blank=True)
-
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)

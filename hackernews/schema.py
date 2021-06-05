2091794b97b228d69a1d6c1eb9fe69ae8413e15a
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-05 17:12:48 -0400

    create link mutation added to backend

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -8,4 +8,8 @@ class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(links.schema.Mutation, graphene, ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- links/schema.py
+++ links/schema.py
@@ -9,8 +9,32 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
+# define mutation class
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # defines data you can send to the server
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # this is the mutation method. it creates a link in the DB and then returns the CreateLink class with the data just created
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(id=link.id, url=link.url, description=link.description)
+
+
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+
+# create a mutation class with a field to be resolved, which points to our mutation defined above
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()

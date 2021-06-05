36bcfc2b19a75fca919606d249c446e05dcde2fb
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-05 10:48:39 -0400

    create LinkType and Query in links schema

--- links/models.py
+++ links/models.py
@@ -5,3 +5,6 @@ class Link(models.Model):
     url = models.URLField()
     # blank=True allows the field to be blank, default is set to False
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.description

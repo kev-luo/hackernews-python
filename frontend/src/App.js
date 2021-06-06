b741e6d7e12449ba6e32e323998348f57ecd0fa9
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-06 13:19:59 -0400

    configure apollo client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from "@apollo/client";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // initialize ApolloClient
+
+  const QUERY_LINKS = gql`
+    query {
+      links {
+        id
+        url
+        description
+      }
+    }
+  `;
+
+  const { data, loading } = useQuery(QUERY_LINKS);
+  if (loading) return <p>Loading...</p>;
+
+  if (data) console.log(data);
+
+  return <div className="App"></div>;
 }
 
 export default App;
